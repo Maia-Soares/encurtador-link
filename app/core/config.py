0243962aeb646a58cad4377565611321eca08738
Maiana Soares <maiana.a.soares@gmail.com>
2026-05-09 18:15:14 -0300

    ajuste formatacao

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=BASE_DIR / ".env", env_file_encoding="utf-8", extra="ignore", case_sensitive=True
+        env_file=BASE_DIR / ".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+        case_sensitive=True,
     )
 
     DB_USER: str
